e104ff141b88a392bea6927df5482fa107f62ebc
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-06-22 01:59:19 +0530

    Update Test_Receive.cpp

--- Test_Receive.cpp
+++ Test_Receive.cpp
@@ -3,7 +3,16 @@
 #include "test/catch.hpp"
 #include "Receive.h"
 
-TEST_CASE("To check if file was successfully opened to read") 
+
+
+
+/*Positive test case */
+TEST_CASE("Case for Postive test case :- Success of Data ReadFromConsole") {
+
+  REQUIRE(ReadFromConsole(Temperature, Charge_rate) == PASS);
+}
+
+/*TEST_CASE("To check if file was successfully opened to read") 
 {
   Sender_InType InVal = FileInput ; 
   REQUIRE(InputValue(InVal) == PASS); 
@@ -15,4 +24,4 @@ TEST_CASE("To check if file was successfully written to console")
   Sender_OutType OutVal = PrintToConsole ;
   REQUIRE(OutputValue(OutVal)== PASS);
   
-}
+}*/
