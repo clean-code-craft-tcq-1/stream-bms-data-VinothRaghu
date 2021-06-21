135dd1611704448c5301a635c491dd1a2f9bb9b9
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-06-22 01:19:22 +0530

    Delete Receiver directory

--- Receiver/Test_Receive.cpp
+++
@@ -1,9 +0,0 @@
-#include "test/catch.hpp"
-#include "Receive.h"
-
-
-/*Positive test case */
-TEST_CASE("Case for Postive test case :- Success of Data ReadFromConsole") {
-
-  REQUIRE(ReadFromConsole(Temperature, Charge_rate) == PASS);
-}
