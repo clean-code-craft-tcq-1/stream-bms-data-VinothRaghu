5f868d95b2442d7bb18a22f34a5cab61d967dba6
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-06-22 01:54:26 +0530

    Update Test_Receive.cpp

--- Test_Receive.cpp
+++ Test_Receive.cpp
@@ -1,9 +1,18 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
 #include "test/catch.hpp"
 #include "Receive.h"
 
+TEST_CASE("To check if file was successfully opened to read") 
+{
+  Sender_InType InVal = FileInput ; 
+  REQUIRE(InputValue(InVal) == PASS); 
+}
 
-/*Positive test case */
-TEST_CASE("Case for Postive test case :- Success of Data ReadFromConsole") {
 
-  REQUIRE(ReadFromConsole(Temperature, Charge_rate) == PASS);
+TEST_CASE("To check if file was successfully written to console") 
+{
+  Sender_OutType OutVal = PrintToConsole ;
+  REQUIRE(OutputValue(OutVal)== PASS);
+  
 }
